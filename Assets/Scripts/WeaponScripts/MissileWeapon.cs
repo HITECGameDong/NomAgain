2291de1425d12dfa14a02f64792a5e4559ea391b
sadjin25 <84575605+sadjin25@users.noreply.github.com>
2025-11-29 13:59:11 +0900

    feat: missile weapon cooltime sys

--- Assets/Scripts/WeaponScripts/MissileWeapon.cs
+++ Assets/Scripts/WeaponScripts/MissileWeapon.cs
@@ -6,32 +6,32 @@ using UnityEngine;
 public class MissileWeapon : Weapon
 {
     [SerializeField] int breakAmount = 5;
+    [SerializeField] float cooltime = 15f;
+    float cooltimeTimer = 0f;
 
-    // 미사일은 습득하자마자 쏩니다.
+    void FixedUpdate()
+    {
+        cooltimeTimer += Time.fixedDeltaTime;
+        if(cooltimeTimer >= cooltime)
+        {
+            cooltimeTimer = 0f;
+            Attack();
+        }   
+    }    
+
     public override void Attack()
     {
-        return;
-    }
-
-    // 미사일은 습득하자마자 쏩니다.
-    public override void WeaponInit(Player player)
-    {
-        SetWeaponUser(player);
+        cooltimeTimer = 0f;
 
         Collider[] everyColliders = Physics.OverlapBox(
-            range.transform.TransformPoint(range.center), 
-            range.bounds.extents,
-            Quaternion.identity,
-            targetLayer);
-        
-        if(everyColliders.Length == 0)
-        {
-            // weaponUser.UnequipWeapon();
-            return;
-        }
+        range.transform.TransformPoint(range.center), 
+        range.bounds.extents,
+        Quaternion.identity,
+        targetLayer);
 
         // 미사일 발사
         int index = 0;
+        breakAmount = 5;
         while(breakAmount > 0 && everyColliders.Length > index)
         {
             everyColliders[index].transform.gameObject.SetActive(false);
@@ -39,15 +39,21 @@ public class MissileWeapon : Weapon
             index++;
             breakAmount--;
             weaponUser.onObstacleBroken.Invoke();
-        }
-
-        // 다쐈으면 무기파괴
-        // weaponUser.UnequipWeapon();
+        }    
     }
 
+    // 미사일은 습득하자마자 쏩니다.
+    public override void WeaponInit(Player player)
+    {
+        SetWeaponUser(player);
+        Attack();
+    }
+
+    // 미사일은 습득하자마자 쏩니다.
     public override void WeaponLevelUp()
     {
         Debug.Log("MISSILE LEVEL UP");
+        Attack();
         weaponLevel++;
     }
 }
