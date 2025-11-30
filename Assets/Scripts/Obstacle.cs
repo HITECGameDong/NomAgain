1cf58410c945c7dd90b52329f1f1a13acfcaae94
sadjin25 <84575605+sadjin25@users.noreply.github.com>
2025-11-30 16:17:21 +0900

    feat: obstacle bk effect

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -2,5 +2,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    [SerializeField] ParticleSystem brokenParticle;
+    bool disableParticle = true;
 
+    void OnDisable()
+    {
+        if(disableParticle)
+        {
+            disableParticle = false;
+            return;    
+        }
+
+        if(brokenParticle != null)
+        {
+            Instantiate(brokenParticle, transform.position, Quaternion.identity);
+        }
+    }
 }
